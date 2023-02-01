422ccfe025cd8536bc88585add1d48931714875e
mm-102 <maciej.kowalski@student.put.poznan.pl>
2023-02-01 21:12:54 +0100

    Add Fireball + bugfix  - Fix LaserBullet killing players

--- Singletons/Ammunition.gd
+++ Singletons/Ammunition.gd
@@ -1,7 +1,7 @@
 extends Reference
 class_name Ammunition
 
-enum TYPES{BULLET, ROCKET, FRAG_BOMB, LASER, LASER_BULLET}
+enum TYPES{BULLET, ROCKET, FRAG_BOMB, LASER, LASER_BULLET, FIREBALL}
 
 
 const _ammo_box_texture = {
@@ -9,7 +9,8 @@ const _ammo_box_texture = {
 	TYPES.ROCKET: preload("res://textures/rocket.png"),
 	TYPES.FRAG_BOMB: preload("res://textures/bullet.png"),
 	TYPES.LASER: preload("res://textures/laser_box.png"),
-	TYPES.LASER_BULLET: preload("res://textures/laser_bullet_box.png")
+	TYPES.LASER_BULLET: preload("res://textures/laser_bullet_box.png"),
+	TYPES.FIREBALL: preload("res://textures/fireball_box.png")
 }
 
 
