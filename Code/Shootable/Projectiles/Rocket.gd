7052a4d5dec205cba06e8e4a6fb44be15f57f239
mm-102 <maciej.kowalski@student.put.poznan.pl>
2023-04-02 14:51:59 +0200

    Send rocket target and owner color

--- Code/Shootable/Projectiles/Rocket.gd
+++ Code/Shootable/Projectiles/Rocket.gd
@@ -6,12 +6,23 @@ var started_targeting = false
 
 var s = GameSettings.Dynamic.Ammunition[Ammunition.TYPES.ROCKET]
 
+var owner_color = Color.white
+var target_color = Color.white
+
+
+func _ready():
+	var player_id = general_info.get_info().PlayerID
+	var player_n = get_node_or_null("/root/Main/Game/Players/" + str(player_id))
+	if player_n != null:
+		owner_color = player_n.player_color
 
 func get_stance() -> Dictionary:
 	var stance = {
 		"ID": name,
 		"P": position,
 		"R": rotation,
+		"OC": owner_color,
+		"TC": target_color
 	}
 	return stance
 
@@ -25,14 +36,17 @@ func _integrate_forces(state):
 	for player in get_tree().get_nodes_in_group("Players"):
 		if target == null:
 			target = player
+			target_color = player.player_color
 		if !is_instance_valid(target) or !target.is_inside_tree():
 			target = null
+			target_color = Color.white
 			continue
 		if !is_instance_valid(player):
 			continue
 		if player.global_position.distance_to(self.global_position) < \
 				target.global_position.distance_to(self.global_position):
 			target = player
+			target_color = player.player_color
 	if is_instance_valid(target):
 		navigation_agent.set_target_location(target.global_position)
 		var move_direction = position.direction_to(navigation_agent.get_next_location())
